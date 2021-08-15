be1dccb50cd1fe6740c785aab712acd1be3de32a
Kevin Cochran <kcochran@hashicorp.com>
2021-08-15 19:37:30 -0400

    add backend

---
+++ backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "remote" {
+    organization = "kevindemos"
+
+    workspaces {
+      name = "javaperks-aws-k8s"
+    }
+  }
+}
