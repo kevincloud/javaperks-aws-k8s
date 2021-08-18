7d087b9e594c631a67f7e87bd9608ef1748c02d4
Kevin Cochran <kcochran@hashicorp.com>
2021-08-17 22:10:50 -0400

    update

--- scripts/06_launch_javaperks.sh
+++ scripts/06_launch_javaperks.sh
@@ -5,6 +5,7 @@ kubectl apply -f /root/javaperks-aws-k8s/scripts/javaperks/auth-api.yaml
 
 # Deploy product-api
 kubectl apply -f /root/javaperks-aws-k8s/scripts/javaperks/product-api.yaml
+kubectl apply -f /root/javaperks-aws-k8s/scripts/javaperks/product-svc.yaml
 
 # Deploy customer-api
 kubectl apply -f /root/javaperks-aws-k8s/scripts/javaperks/customer-api.yaml
@@ -12,20 +13,16 @@ kubectl apply -f /root/javaperks-aws-k8s/scripts/javaperks/customer-api.yaml
 # Deploy cart-api
 kubectl apply -f /root/javaperks-aws-k8s/scripts/javaperks/cart-api.yaml
 
-# Deploy cart-api
+# Deploy order-api
 kubectl apply -f /root/javaperks-aws-k8s/scripts/javaperks/order-api.yaml
 
 # Deploy store-api
 kubectl apply -f /root/javaperks-aws-k8s/scripts/javaperks/online-store.yaml
+kubectl apply -f /root/javaperks-aws-k8s/scripts/javaperks/front-end.yaml
 
 # Deploy ldap
 kubectl apply -f /root/javaperks-aws-k8s/scripts/javaperks/openldap.yaml
-
-# Create ldap LB
 kubectl apply -f /root/javaperks-aws-k8s/scripts/javaperks/ldap-front-end.yaml
-
-# Create front-end LB
-kubectl apply -f /root/javaperks-aws-k8s/scripts/javaperks/front-end.yaml
 
 # Create intentions
 kubectl exec hc-consul-consul-server-0 -- curl \
